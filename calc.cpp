e10f0c06d16dcdf1c5381d7bb756de28b500235b
Brandon Stewart <stewartb2@gmail.com>
2016-09-28 22:10:00 -0400

    Added basic operations

---
+++ calc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+int add(int num1, int num2) {
+	return num1 + num2;
+}
+
+int subtract(int num1, int num2) {
+	return num1 - num2;
+}
+
+int multiply(int num1, int num2) {
+	return num1 * num2;
+}
+
+//need to implement
+int divide(int num1, int num2) {
+}
+
+
+int main() {
+
+int num1 = 50;
+int num2 = 30;
+
+cout << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
+cout << num1 << " - " << num2 << " = " << subtract(num1, num2) << endl;
+cout << num1 << " * " << num2 << " = " << multiply(num1, num2) << endl;
+
+return 0;
+}
